ae9da819adf36a2c7ce08955456adf94f89b9370
epozo <31476095+epozo@users.noreply.github.com>
2017-11-03 11:53:03 +0100

    Segunda versión de mi programa

--- ProvesGit/ProvesGit/src/rpovesGit/ProgramaGit.java
+++ ProvesGit/ProvesGit/src/rpovesGit/ProgramaGit.java
@@ -4,7 +4,8 @@ public class ProgramaGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Estoy vivi....!!!!");
+		System.out.println("Estoy vivo....!!!!");
+		System.out.println("Qué alegría....!!!!");
 	}
 
 }
