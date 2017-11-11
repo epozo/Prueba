e99683e97ca124df5738fb9b4a25e71a6a698769
Elena <epozo@ies-sabadell.cat>
2017-11-11 20:39:55 +0100

    11/11/2017 - Rama 1

--- ProvesGit/ProvesGit/src/rpovesGit/ProgramaGit.java
+++ ProvesGit/ProvesGit/src/rpovesGit/ProgramaGit.java
@@ -3,10 +3,10 @@ package rpovesGit;
 public class ProgramaGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Estoy vivo....!!!!");
 		System.out.println("Qué alegría....!!!!");
-		System.out.println("Y ahora qué  ....!!!!");
+		
 		
 	}
 
